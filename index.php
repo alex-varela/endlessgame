4ad83215d3446306f26ba256acef27b58db28044
alex <afvarela93@gmail.com>
2021-06-28 07:37:58 -0600

    Update game

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
 <?php require_once('./UnoGame/index.html');?>
+
+
+<!-- /**
+ <div id="unity-mobile-warning">
+       
+       <a href="https://www.uno-terra.com/">Participa en la rifa has click aqui</a>
+             </div>
+*/ -->

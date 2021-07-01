a3185b189241d4769ca57100de1cac977cc38148
alex <afvarela93@gmail.com>
2021-06-30 23:48:09 -0600

    update

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 
 <!-- /**
       <div id="unity-mobile-warning">
-       
         <a href="https://www.uno-terra.com/aventurauno/hn" id="text">RECLAMA TU PREMIO</a>
-              </div>
+      </div>
 */ -->
